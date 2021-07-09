e2691c6cb3855cb7aec5e82889bb6ec8206897a6
VivekG11 <vivekvk2903@gmail.com>
2021-07-09 17:31:55 +0530

    [Find Max int Value among three]

---
+++ FindMaxOfThree/FindMaxValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxOfThree
+{
+    class FindMaxValue
+    {
+        public int MaxInt(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("All are equal...");
+        }
+
+    }
+}
---
+++ FindMaxOfThree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FindMaxOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Of Three........!");
+            FindMaxValue maxValue = new FindMaxValue();
+            int maximum = maxValue.MaxInt(11, 13, 29);
+            Console.WriteLine("Maximum Value among three is :"+maximum);
+        }
+    }
+}
